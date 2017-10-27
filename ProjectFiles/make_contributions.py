fce5235fd0db4dea287909b4fb93bfe343592e17
oaktrunks <TomeiDLT@gmail.com>
2017-10-27 11:39:54 -0500

    Improved make_contributions.py

--- ProjectFiles/make_contributions.py
+++ ProjectFiles/make_contributions.py
@@ -5,21 +5,26 @@ import random
 from faker import Faker
 fake = Faker()
 
-fund_names = ["Comfort Dog", "No Longer Active", "Scholarship",
-              "Youth Camp Fees", "Youth Fundraisers", "Youth, Other"]
-
-c_types = ["ach", "coin", "check", "currency"]
-
-env_num_limit = 50
+#########################################################################
+#These are hardcoded. copied from a select statement in phpMyAdmin      #
+fund_names = ["Comfort Dog", "No Longer Active", "Scholarship",         #
+              "Youth Camp Fees", "Youth Fundraisers", "Youth, Other"]   #
+                                                                        #
+c_types = ["ach", "coin", "check", "currency"]                          #
+                                                                        #
+env_num_limit = 50 #Highest envelope number in database                 #
+                                                                        #
+#End of hard coding                                                     #
+#########################################################################
 
 with open("make_contributions.sql", "w+") as file:
     for i in range(50):
         amt = random.randint(500, 99999999) / 100
         c_date = "'{}-{}-{}'".format(random.randint(2000, 2017), random.randint(1, 12), random.randint(1,29))
         note = "'{}'".format(fake.sentence())
-        c_type = "'{}'".format(c_types[random.randint(0,3)]) #HARDCODED
-        fund_name = "'{}'".format(fund_names[random.randint(0,5)]) #HARDCODED
-        env_num = random.randint(0,50)
+        c_type = "'{}'".format(c_types[random.randint(0,len(c_types)-1)])
+        fund_name = "'{}'".format(fund_names[random.randint(0,len(fund_names)-1)])
+        env_num = random.randint(0,env_num_limit)
 
         file.write("INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num)")
         file.write(" VALUES({}, {}, {}, {}, {}, {});\n\n".format( amt, c_date, note, c_type, fund_name, env_num))
