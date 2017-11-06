c635678ca768f73ba049074d9b23bc26ad8392f6
oaktrunks <TomeiDLT@gmail.com>
2017-11-06 11:02:04 -0600

    Update make_contributions.sql

--- ProjectFiles/make_contributions.sql
+++ ProjectFiles/make_contributions.sql
@@ -1,100 +1,100 @@
-INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(256, '2001-2-15', 'Explicabo necessitatibus esse corrupti laudantium enim inventore.', 'check', 'Facility Use', 24);
+INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(261.28, '2010-7-18', 'Voluptatibus accusamus voluptate debitis incidunt dolorem.', 'currency', 'No Longer Active', 10);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(67, '2005-6-2', 'check', 'Scholarship', 31);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(48.56, '2001-2-2', 'currency', 'Bulletins', 5);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(86, '2008-1-11', 'check', 'Facility Use', 14);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(62.66, '2010-7-5', 'check', 'Youth Fundraisers', 14);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(89, '2006-3-23', 'ach', 'Youth Fundraisers', 9);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(90.13, '2010-6-6', 'coin', 'General', 8);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(71, '2004-8-9', 'currency', 'Bulletins', 48);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(29.23, '2016-6-1', 'ach', 'Missions', 20);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(73, '2016-6-28', 'currency', 'Music', 6);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(58.03, '2010-6-16', 'ach', 'Youth, Other', 34);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(269, '2011-1-28', 'check', 'Bulletins', 32);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(233.63, '2016-12-6', 'check', 'Scholarship', 15);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(93, '2001-8-18', 'check', 'No Longer Active', 2);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(85.8, '2014-3-23', 'ach', 'Bulletins', 18);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(20, '2011-8-22', 'check', 'Comfort Dog', 27);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(17.62, '2014-4-4', 'currency', 'General', 40);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(76, '2013-12-11', 'ach', 'Facility Use', 19);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(48.83, '2004-12-21', 'coin', 'No Longer Active', 31);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(65, '2011-7-14', 'coin', 'Missions', 3);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(38.54, '2006-12-18', 'ach', 'Comfort Dog', 45);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(82, '2010-10-14', 'currency', 'Music', 30);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(8.13, '2006-10-22', 'currency', 'Youth, Other', 50);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(282, '2014-8-12', 'check', 'Bulletins', 28);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(210.81, '2016-8-11', 'currency', 'Missions', 39);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(83, '2013-5-19', 'ach', 'Comfort Dog', 34);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(32.59, '2015-3-27', 'coin', 'Scholarship', 4);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(22, '2012-11-14', 'ach', 'Facility Use', 22);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(2.61, '2003-7-15', 'coin', 'Youth Camp Fees', 49);
 
-INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(84, '2012-7-4', 'Quod saepe consequuntur dolores rerum autem.', 'ach', 'Building', 29);
+INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(1.83, '2014-7-27', 'Provident modi enim qui.', 'coin', 'Bulletins', 37);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(5, '2007-12-27', 'check', 'Missions', 9);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(26.0, '2014-3-9', 'currency', 'No Longer Active', 43);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(35, '2006-1-21', 'currency', 'Youth Camp Fees', 48);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(49.48, '2007-5-16', 'currency', 'Youth, Other', 5);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(210, '2001-3-3', 'coin', 'Building', 18);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(283.74, '2017-11-20', 'ach', 'Facility Use', 42);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(4, '2016-5-11', 'coin', 'No Longer Active', 8);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(85.85, '2001-11-3', 'currency', 'Youth, Other', 42);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(58, '2015-8-18', 'coin', 'Music', 46);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(45.59, '2009-5-16', 'check', 'Youth, Other', 29);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(83, '2006-10-17', 'coin', 'Music', 15);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(24.7, '2001-12-20', 'currency', 'General', 20);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(49, '2016-9-28', 'check', 'Youth Camp Fees', 31);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(28.09, '2004-5-23', 'check', 'General', 4);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(25, '2001-1-15', 'coin', 'Youth Camp Fees', 33);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(9.45, '2012-5-9', 'coin', 'No Longer Active', 49);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(239, '2008-10-18', 'coin', 'Facility Use', 34);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(246.94, '2000-10-16', 'check', 'No Longer Active', 39);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(45, '2006-12-25', 'coin', 'Youth, Other', 5);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(54.03, '2013-9-19', 'currency', 'Youth Fundraisers', 42);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(36, '2010-4-20', 'currency', 'Music', 7);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(70.7, '2015-1-23', 'coin', 'General', 1);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(91, '2010-11-29', 'check', 'Youth, Other', 26);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(55.56, '2015-1-24', 'coin', 'Youth, Other', 9);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(59, '2015-10-22', 'check', 'Building', 43);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(19.71, '2004-5-5', 'check', 'Facility Use', 21);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(86, '2003-12-17', 'ach', 'Music', 27);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(87.37, '2004-11-27', 'coin', 'Building', 39);
 
-INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(209, '2003-6-25', 'Blanditiis rem consequatur consequuntur molestias dolorem fugit.', 'coin', 'General', 24);
+INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(238.61, '2013-6-25', 'Culpa incidunt placeat fugiat nulla.', 'ach', 'Youth Camp Fees', 17);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(8, '2001-1-24', 'ach', 'Missions', 40);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(95.32, '2007-7-21', 'ach', 'Youth, Other', 3);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(73, '2006-7-5', 'check', 'Comfort Dog', 19);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(22.6, '2003-5-11', 'ach', 'Bulletins', 25);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(55, '2007-10-20', 'ach', 'Scholarship', 30);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(72.46, '2017-9-8', 'ach', 'Youth Fundraisers', 33);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(9, '2006-8-14', 'coin', 'General', 21);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(40.45, '2015-12-17', 'ach', 'Building', 6);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(10, '2015-5-1', 'check', 'Building', 17);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(51.64, '2013-6-27', 'coin', 'Youth, Other', 22);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(284, '2009-5-19', 'ach', 'Comfort Dog', 30);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(205.32, '2002-2-23', 'coin', 'General', 9);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(62, '2016-5-28', 'ach', 'Music', 32);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(1.49, '2004-4-2', 'currency', 'Building', 34);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(29, '2006-1-26', 'check', 'Bulletins', 36);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(91.35, '2009-11-29', 'ach', 'Youth Fundraisers', 48);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(40, '2014-1-17', 'currency', 'Music', 30);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(96.83, '2000-5-23', 'currency', 'Missions', 12);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(42, '2004-11-28', 'ach', 'Facility Use', 12);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(12.4, '2015-8-13', 'ach', 'Facility Use', 28);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(52, '2012-12-28', 'ach', 'Music', 42);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(14.97, '2017-6-26', 'currency', 'General', 13);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(261, '2012-12-13', 'currency', 'Comfort Dog', 25);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(209.07, '2007-4-19', 'currency', 'Facility Use', 46);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(19, '2017-1-26', 'ach', 'Youth Camp Fees', 50);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(35.89, '2009-12-6', 'currency', 'Comfort Dog', 36);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(16, '2011-2-29', 'ach', 'Music', 50);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(75.68, '2005-3-13', 'coin', 'Scholarship', 13);
 
-INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(1, '2015-11-20', 'Blanditiis eius unde quos nesciunt.', 'ach', 'Bulletins', 18);
+INSERT INTO contribution(amt, c_date, note, c_type, fund_name, env_num) VALUES(12.04, '2006-8-18', 'Soluta praesentium qui at error magnam suscipit corrupti.', 'ach', 'No Longer Active', 39);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(87, '2007-9-28', 'ach', 'Scholarship', 48);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(45.19, '2014-8-21', 'check', 'Comfort Dog', 11);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(36, '2005-10-16', 'ach', 'Youth Camp Fees', 8);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(98.73, '2015-6-26', 'coin', 'General', 8);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(209, '2016-4-13', 'currency', 'Youth Fundraisers', 19);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(292.93, '2014-10-1', 'currency', 'Youth Fundraisers', 17);
 
-INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(45, '2012-4-12', 'ach', 'Youth Fundraisers', 1);
+INSERT INTO contribution(amt, c_date, c_type, fund_name, env_num) VALUES(18.93, '2007-1-5', 'ach', 'Youth Camp Fees', 3);
 
